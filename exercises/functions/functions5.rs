2350e9e84765abdeba97b64035ff812529571358
Shriram Balaji <shrirambalaji1996@gmail.com>
2020-09-05 20:27:27 +0530

    Update excercises

--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -1,13 +1,14 @@
 // functions5.rs
 // Make me compile! Execute `rustlings hint functions5` for hints :)
 
-// I AM NOT DONE
 
 fn main() {
     let answer = square(3);
     println!("The answer is {}", answer);
 }
 
+// expressions always return
+// when there's a semicolon its a statement, and doesnt return anything
 fn square(num: i32) -> i32 {
-    num * num;
+    num * num
 }
